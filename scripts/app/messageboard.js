a8a628a7c41df15a673482e07becf519567dcb0a
Bart Bakker <bart@thesoftwarecraft.com>
2013-05-15 23:28:41 +0200

    connect message board list to REST service
    Add a MessageBoard service to load messages from the REST services using
    Angular's $http service.
    
    Provide an Angular factory for the 'MessageBoard' service so that it can
    be used by Angular's dependency injection and therefore can be injected
    in the controller.

--- scripts/app/messageboard.js
+++ scripts/app/messageboard.js
@@ -1,4 +1,29 @@
 angular.module('koans.messageboard', [])
-  .controller('MessageBoardController', ['$scope', function($scope) {
-    $scope.messages = ['msg-1', 'msg-2', 'msg-3']
+  .controller('MessageBoardController', ['$scope', 'MessageBoard', function($scope, messageBoard) {
+    $scope.messages = messageBoard.messages();
+  }])
+  .factory('MessageBoard', ['$http', function($http) {
+    return MessageBoard.create($http);
   }]);
+
+var MessageBoard = {
+  create: function($http) {
+    return Object.create(MessageBoard, {
+        $http: { value: $http }
+    })
+  },
+
+  messages: function() {
+    return this.$http.get('/api/messageboard/list').then(function(response) {
+      return response.data.map(MessageData.load)
+    });
+  },
+}
+
+var MessageData = {
+  load: function(data) {
+    return { id: data.id,
+             text: data.message }
+  }
+}
+
