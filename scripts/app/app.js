c38f9c1b90dda8495a2aa23690f55e582343ca7e
Bart Bakker <bart@thesoftwarecraft.com>
2013-05-15 12:00:53 +0200

    set up '/home' route and view
    Configure the 'koans' angular module with a $routeProvider. Using this
    provider application routes are set up.
    
    As '/home' is visited the view template in 'views/home.html' is rendered
    by Angular.
    
    On any unknown route the application redirects to '/home'

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,4 +1,8 @@
 angular.module('koans', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', { templateUrl: 'views/home.html' });
+    $routeProvider.otherwise({redirectTo: '/home'});
+  }])
   .run(['$rootScope', function($rootScope) {
     $rootScope.app = {
       name: 'Angular Koans'
