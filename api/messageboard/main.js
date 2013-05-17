53a3de96ac9639e35a2265f0d60e7caa83dd1a43
Bart Bakker <bart@thesoftwarecraft.com>
2013-05-17 22:45:25 +0200

    fix duplicate mesasge id

--- api/messageboard/main.js
+++ api/messageboard/main.js
@@ -4,7 +4,7 @@ var fixedMessageStore = {
   messages: function() {
     return [{id: 1, message: "Welcome to the message board"},
             {id: 2, message: "You've round tripped to the server and back"},
-            {id: 2, message: "Well done!"}];
+            {id: 3, message: "Well done!"}];
   }
 }
 var messageBoard = Object.create(MessageBoard);
