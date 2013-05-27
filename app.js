43a7afa1dd1b9e9dc700c37f8adb2c745bd9d998
Bart Bakker <bart@thesoftwarecraft.com>
2013-05-27 23:22:59 +0200

    move UI resources into public/

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 function main(terminal) {
   var app = express();
   app.set('port', process.env.PORT || 8080);
-  app.use(express.static(__dirname));
+  app.use(express.static(__dirname + '/public'));
   app.get('/api/messageboard/list', routes.messageboard.list);
   http.createServer(app).listen(app.get('port'), function() {
     terminal.info("Server started on port: %s", app.get('port'));
